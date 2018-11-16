04255264b1abc17c4400cacc00f785ac9e4835c1
anjalisg <anjalisgopinathan@gmail.com>
2018-11-16 13:24:52 -0800

    color sensor code

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_TestingColorSensorTape_BLUE.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_TestingColorSensorTape_BLUE.java
@@ -9,20 +9,35 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class Auto_TestingColorSensorTape_BLUE extends LinearOpMode{
     Hardware9837_geary_v3 geary = new Hardware9837_geary_v3();
 
+
     public void runOpMode() {
+        int moveTime=100;    //miliseconds
+
         geary.init(hardwareMap);
 
         waitForStart();
+
+
+        telemetry.addData("left RGB: (" + geary.colorSensorLeft.red() + ", " + geary.colorSensorLeft.green() + ", " + geary.colorSensorLeft.blue() + ")", 0);
+        telemetry.addData("right RGB: (" + geary.colorSensorRight.red() + ", " + geary.colorSensorRight.green() + ", " + geary.colorSensorRight.blue() + ")", 0);
+
         geary.leftMotor.setPower(1.0);
         geary.rightMotor.setPower(1.0);
 
-        telemetry.addData("left RGB: (" + geary.colorSensorLeft.red() + ", " + geary.colorSensorLeft.green() + ", " + geary.colorSensorLeft.blue()  +")", 0);
-        telemetry.addData("right RGB: (" + geary.colorSensorRight.red() + ", " + geary.colorSensorRight.green() + ", " + geary.colorSensorRight.blue()  +")", 0);
-        sleep(2000);
+        //While left blue is small, keep moving forward.
+        while (     (geary.colorSensorLeft.blue() < 150)// && !(geary.colorSensorLeft.blue()>geary.colorSensorLeft.red() && geary.colorSensorLeft.blue() > geary.colorSensorLeft.green()))
+                &&  (geary.colorSensorRight.blue() < 150)// && geary.colorSensorRight.blue()>geary.colorSensorRight.red() && geary.colorSensorRight.blue() > geary.colorSensorRight.green())
+              ){
+        // If the blue value is low on both sensors
+            moveTime+=100;
+        }
+        sleep(moveTime);
+
 
 
         geary.leftMotor.setPower(0.0);
         geary.rightMotor.setPower(0.0);
+        sleep(1000);
     }
 
 
--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware9837_geary_v3.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware9837_geary_v3.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -12,33 +13,56 @@ import com.qualcomm.robotcore.hardware.Servo;
 class Hardware9837_geary_v3 {
     DcMotor rightMotor;
     DcMotor leftMotor;
+    
     ColorSensor colorSensorLeft;
     ColorSensor colorSensorRight;
+    
     Servo markerPusher;
+    
+    DcMotor intakeMotor;
+    DcMotor armMotor;
+    DcMotor extendIntakeMotor;
+    
+    DcMotor liftUpMotor;
 
-    HardwareMap hwMap = null;
+    //HardwareMap hwMap = null;
     //private ElapsedTime period  = new ElapsedTime();
 
-    public void init(HardwareMap hardwareMap){
+    public void init(HardwareMap hwMap){
         //set direction of motors facing opposite directions
-        rightMotor = hardwareMap.dcMotor.get("rightDrive");
-        leftMotor = hardwareMap.dcMotor.get("leftDrive");
+        rightMotor = hwMap.dcMotor.get("rightDrive");
+        leftMotor = hwMap.dcMotor.get("leftDrive");
 
-        colorSensorLeft = hardwareMap.colorSensor.get("colorSensorLeft");
-        colorSensorRight = hardwareMap.colorSensor.get("colorSensorRight");
+        colorSensorLeft = hwMap.colorSensor.get("colorSensorLeft");
+        colorSensorRight = hwMap.colorSensor.get("colorSensorRight");
 
-//        markerPusher = hardwareMap.servo.get("marker pusher");
-//        markerPusher.setPosition(0);
+        markerPusher = hwMap.servo.get("marker pusher");
+        markerPusher.setPosition(0);
+        
+        intakeMotor = hwMap.dcMotor.get("intake");
+        armMotor = hwMap.dcMotor.get("arm");
+        extendIntakeMotor = hwMap.dcMotor.get("extend intake");
 
+        
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
         leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        
+        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
+        armMotor.setDirection(DcMotor.Direction.FORWARD);
+        extendIntakeMotor.setDirection(DcMotor.Direction.FORWARD);
 
         rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        
         //set pwr to 0
-
         rightMotor.setPower(0.0);
         leftMotor.setPower(0.0);
+        intakeMotor.setPower(0.0);
+        armMotor.setPower(0.0);
+        extendIntakeMotor.setPower(0.0);
+        liftUpMotor.setPower(0.0);
+        
+        
 
     }
 }
