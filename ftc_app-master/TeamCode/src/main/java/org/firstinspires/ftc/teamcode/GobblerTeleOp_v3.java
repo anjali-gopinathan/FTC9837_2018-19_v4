7271333e21c6d184932d3f0133c98767c02f9e2c
anjalisg <anjalisgopinathan@gmail.com>
2018-12-01 22:41:16 -0800

    Oops forgot to push after today's scrimmage

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GobblerTeleOp_v3.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GobblerTeleOp_v3.java
@@ -20,37 +20,70 @@ public class GobblerTeleOp_v3 extends LinearOpMode{
             double leftDrive = gamepad1.left_stick_y;
             double rightDrive = gamepad1.right_stick_y;
 
-            boolean armMotorUp = gamepad1.dpad_up;
-            boolean armMotorDown = gamepad1.dpad_down;
+            double armMotorUp = -gamepad1.right_trigger;
+            double armMotorDown = -gamepad1.left_trigger;
 
             boolean extendMotorOut = gamepad1.b;
             boolean extendMotorIn = gamepad1.x;
+
+            boolean intakeIn = gamepad1.left_bumper;
+            boolean intakeOut = gamepad1.right_bumper;
+
+            boolean markerPusherIn = gamepad1.y;
+            boolean markerPusherOut = gamepad1.a;
 
             double motorPower = 0.8;
 
             gobbler.leftMotor.setPower(leftDrive);
             gobbler.rightMotor.setPower(rightDrive);
 
-            if(armMotorUp) {
-                gobbler.armMotorLeft.setPower(motorPower);
-                gobbler.armMotorRight.setPower(motorPower);
+            if(armMotorUp >0) {
+                gobbler.armMotorLeft.setPower(armMotorUp);
+                gobbler.armMotorRight.setPower(armMotorUp);
             }
-            else if (armMotorDown) {
-                gobbler.armMotorLeft.setPower(-motorPower);
-                gobbler.armMotorRight.setPower(-motorPower);
+            else if (armMotorDown >0) {
+                gobbler.armMotorLeft.setPower(armMotorDown);
+                gobbler.armMotorRight.setPower(armMotorDown);
             }
             else {
                 gobbler.armMotorLeft.setPower(0.0);
                 gobbler.armMotorRight.setPower(0.0);
             }
 
-
-            if(extendMotorOut)
+            //extend motor (rack and pinion) - b and x
+            if(extendMotorOut) {
                 gobbler.extendIntakeMotor.setPower(motorPower);
-            else if (extendMotorIn)
+            }
+            else if (extendMotorIn) {
                 gobbler.extendIntakeMotor.setPower(-motorPower);
-            else
+            }
+            else {
                 gobbler.extendIntakeMotor.setPower(0.0);
+            }
+
+            //intake - right and left bumpers
+            if (intakeIn){
+                gobbler.intakeMotor.setPower(1.0);
+            }
+            else if (intakeOut){
+                gobbler.intakeMotor.setPower(-1.0);
+            }
+            else {
+                gobbler.intakeMotor.setPower(0.0);
+            }
+
+            //Servo to push marker into depot- y and a
+            if (markerPusherIn){
+                gobbler.markerPusher.setPosition(gobbler.markerPusher.getPosition()+0.05);
+            }
+            else if (markerPusherOut){
+                gobbler.markerPusher.setPosition(gobbler.markerPusher.getPosition()-0.05);
+            }
+            else {
+                gobbler.markerPusher.setPosition(gobbler.markerPusher.getPosition());
+            }
+
+
 
         }
     }
--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware9837_gobbler.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware9837_gobbler.java
@@ -13,48 +13,47 @@ import com.qualcomm.robotcore.hardware.Servo;
 class Hardware9837_gobbler {
     DcMotor rightMotor;
     DcMotor leftMotor;
-    
-//    ColorSensor colorSensorLeft;
-//    ColorSensor colorSensorRight;
-//
-//    Servo markerPusher;
-    
-//    DcMotor intakeMotor;
+
+    DcMotor intakeMotor;
     DcMotor armMotorLeft;
     DcMotor armMotorRight;
     DcMotor extendIntakeMotor;
-//    DcMotor rotateIntakeMotor;
+
+
+//    ColorSensor colorSensorLeft;
+//    ColorSensor colorSensorRight;
+//
+    Servo markerPusher;
+
 
     //HardwareMap hwMap = null;
     //private ElapsedTime period  = new ElapsedTime();
 
     public void init(HardwareMap hwMap){
         //set direction of motors facing opposite directions
-        rightMotor = hwMap.dcMotor.get("rightDrive");
-        leftMotor = hwMap.dcMotor.get("leftDrive");
+        rightMotor = hwMap.dcMotor.get("rightDrive");   //port zero hub A
+        leftMotor = hwMap.dcMotor.get("leftDrive");     //port 1 hub A
 
 //        colorSensorLeft = hwMap.colorSensor.get("colorSensorLeft");
 //        colorSensorRight = hwMap.colorSensor.get("colorSensorRight");
 //
-//        markerPusher = hwMap.servo.get("marker pusher");
-//        markerPusher.setPosition(0);
+        markerPusher = hwMap.servo.get("marker pusher");
+        markerPusher.setPosition(0);
 //
-//        intakeMotor = hwMap.dcMotor.get("intake");
-        armMotorLeft = hwMap.dcMotor.get("arm left motor");
-        armMotorRight = hwMap.dcMotor.get("arm right motor");
-        extendIntakeMotor = hwMap.dcMotor.get("extend intake");
-//        rotateIntakeMotor = hwMap.dcMotor.get("rotate intake");
+        intakeMotor = hwMap.dcMotor.get("intake");          //port 0 hub b
+        armMotorLeft = hwMap.dcMotor.get("arm left motor"); // port 2 hub A
+        armMotorRight = hwMap.dcMotor.get("arm right motor");//port 3 hub A
+        extendIntakeMotor = hwMap.dcMotor.get("extend intake");//port 1 hub b
 
         
         rightMotor.setDirection(DcMotor.Direction.FORWARD);
         leftMotor.setDirection(DcMotor.Direction.REVERSE);
         
-//        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
+        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
         armMotorLeft.setDirection(DcMotor.Direction.FORWARD);
         armMotorRight.setDirection(DcMotor.Direction.REVERSE);
 
         extendIntakeMotor.setDirection(DcMotor.Direction.FORWARD);
-//        rotateIntakeMotor.setDirection(DcMotor.Direction.FORWARD);
 
 //        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 //        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -62,12 +61,11 @@ class Hardware9837_gobbler {
         //set pwr to 0
         rightMotor.setPower(0.0);
         leftMotor.setPower(0.0);
-//        intakeMotor.setPower(0.0);
+        intakeMotor.setPower(0.0);
         armMotorLeft.setPower(0.0);
         armMotorRight.setPower(0.0);
         extendIntakeMotor.setPower(0.0);
-//        rotateIntakeMotor.setPower(0.0);
-        
+
         
 
     }
